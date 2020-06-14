06356d471c2ed4f962540969b2f719e9a4734237
chuyuxuan123 <chuyuxuan@126.com>
2020-06-14 22:52:24 +0800

    update trader gateway

--- src/main/java/icyfrog/demo/DemoApplication.java
+++ src/main/java/icyfrog/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package icyfrog.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -22,6 +23,8 @@ public class DemoApplication {
     @Bean
     public RouteLocator myRoutes(RouteLocatorBuilder builder, UriConfiguration uriConfiguration)  {
         String httpUri = uriConfiguration.getHttpbin();
+        String fudeUserServer = uriConfiguration.getFudeUserServer();
+        System.out.println("fude user: " + fudeUserServer);
         return builder.routes()
                 // TODO:按照这个格式写就可以了，.path的路径要和.uri的一样，就会从gateway跳转到orderProcess
                 .route(p -> p
@@ -36,6 +39,12 @@ public class DemoApplication {
                         .path("/test/redisSet")
                         //.filters(f -> f.addRequestHeader("Router","Gateway"))
                         .uri( httpUri +"/test/redisSet"))
+                .route(p -> p
+                        .path("/auth/trader/login")
+                        .uri( fudeUserServer + "/auth/trader/login"))
+                .route(p -> p
+                        .path("/auth/trader/register")
+                        .uri( fudeUserServer + "/auth/trader/register"))
                 // TODO: WebSocket need to be done!
                 .route("websocket_route", r -> r.path("/webSocket")
                         .uri("ws://127.0.0.1:6605"))
@@ -50,11 +59,22 @@ class UriConfiguration {
     // TODO: 后端地址有变的时候更改这里就可以
     private String httpbin = "http://192.168.254.148:8089";
 
+    @Value("${fudeUserServer}")
+    private String fudeUserServer;
+
     public String getHttpbin() {
         return httpbin;
     }
 
     public void setHttpbin(String httpbin) {
         this.httpbin = httpbin;
+    }
+
+    public String getFudeUserServer() {
+        return fudeUserServer;
+    }
+
+    public void setFudeUserServer(String fudeUserServer) {
+        this.fudeUserServer = fudeUserServer;
     }
 }
